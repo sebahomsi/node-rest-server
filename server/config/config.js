5c2cce464382042713fdb2bea95851c19e57bf49
Sebastian <sebahomsi@gmail.com>
2020-04-14 13:35:06 -0300

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,21 @@
-//---------
+//===============
 //Puerto
-//---------
+//==============
 process.env.PORT = process.env.PORT || 3000;
+
+//===============
+//Entorno
+//===============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//===============
+//Base de datos
+//===============
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://sebahomsi:8xNr6jDtnfD6D0vT@sebahomsi69-3bamw.mongodb.net/cafe'
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+
+
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
@@ -9,37 +13,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+//requiere y usa la ruta de usuaio
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get World');
-});
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}, (err, res) => {
+    if (err) throw err;
 
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "el nombre es necesario"
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete World');
+    console.log('DB online');
 });
 
 app.listen(process.env.PORT, () => {
